87e328ca334b288a5e634933e47a89582d1759e9
vladqazxczakladqa <124983819+vladqazxczakladqa@users.noreply.github.com>
2023-02-10 10:32:20 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+print text  ="Hello, Python!"
+print text [0]
+print text[0:5]
+print text [4:10]
+print text [0:14]
+print text[7:]
+print text [:5]
+print text [:]
+print text [-1]
+print text [-1:-14]
+print text[::2]
+print text[::-1]
+print text+ "Nice to code you"
+print text[-1]
+my_tuple =(True, 786, 3.24, 'text ',70.2)
+second_tuple =(123< "text")
+print my_tuple
+rint  my_tuple[0]
+print second_tuple
+print my_tuple +second_tuple
+my_dist =()
+my_dist["country"] = "Mexico"
+print my  ["country"]
+another_dist = ("number",my_list" :[1,2,3])
+print another_dict.keys()
+print another_dict.values()
