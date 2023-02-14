7374aa31330dff0272dd50b07be136985a8e1e66
vladqazxczakladqa <124983819+vladqazxczakladqa@users.noreply.github.com>
2023-02-14 13:56:19 +0300

    Add files via upload
    1 ПРАКТИЧЕСКАЯ

---
+++ main (1).py
@@ -0,0 +1,15 @@
+tseloe = 10
+drobnoe = 8.4
+stroka ='no'
+stroka2 = 'Yes'
+stroka3 = '_'
+big_netseloe = tseloe *3.5
+drobnoe = drobnoe -1
+stroka = stroka * 2
+stroka2 = stroka2 *3 stroka = stroka + stroka3+stroka2
+print (tseleo/drobnoe)
+print (big_netseloe / drobnoe)
+print (tseloe)
+print (drobnoe)
+print (stroka)
+print (big_netseloe)
