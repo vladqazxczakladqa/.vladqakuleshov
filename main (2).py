c5bb9c281eb0691e37d92660b6c6b89a934e6193
vladqazxczakladqa <124983819+vladqazxczakladqa@users.noreply.github.com>
2023-02-14 14:26:31 +0300

    Add files via upload
    практическая 2

---
+++ main (2).py
@@ -0,0 +1,16 @@
+a =1 
+b = 2 
+print ((a>b) and (b>a))
+print((b>a) and (a<b))
+print((a>b) or (b>a))
+print(not(b>a))
+c = a + B
+print((c > b) or (b>a))
+print((a>c) or (b>c))
+print((c<b) or (b<a)
+print((a<c) or (b>a))
+f = 'zxc'
+q = 'qwe'
+print((f > q) and (q>f))
+print((q>f) or (q<f)
+print(not(f<q)
